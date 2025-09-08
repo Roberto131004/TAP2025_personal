7ebc28d6fe374981a394037cad82df138eacf45b
Roberto Moreno Rodriguez <refrymanjr@gmail.com>
2025-09-07 21:13:05 -0600

    programacion generica

--- src/Views/CajeroAutomatico.java
+++ src/Views/CajeroAutomatico.java
@@ -2,15 +2,16 @@ package Views;
 
 import Controller.CajeroController;
 import Models.CajeroModels;
+import Models.CajeroModels;
+
 
 public class CajeroAutomatico {
+
     public static void main(String[] args) {
 
-        CajeroModels model= new CajeroModels();
-        CajeroView view= new CajeroView();
-        CajeroController controller= new CajeroController(model, view);
+        CajeroModels model = new CajeroModels();
+        CajeroView view = new CajeroView();
+        CajeroController controller = new CajeroController(model, view);
         controller.iniciarSistema();
-
-
     }
 }
